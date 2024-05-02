35958d1e2d51dfbb55ee312a0ced160a7ab8bc2d
nikkieke <nikkieke001@gmail.com>
2024-05-02 14:01:11 +0100

    chore: update path

---
+++ lib/src/rive_animated_icon.dart
@@ -0,0 +1,102 @@
+import 'package:rive/rive.dart';
+import 'package:flutter/material.dart';
+import '../rive_animated_icon.dart';
+
+/// [RiveAnimatedIcon] is used to render animated icons with different parameters and callbacks
+class RiveAnimatedIcon extends StatefulWidget {
+  const RiveAnimatedIcon({
+    super.key,
+    this.height = 20,
+    this.width = 20,
+    this.color = Colors.black,
+    required this.riveIcon,
+    this.onTap,
+    this.onHover,
+    this.loopAnimation = false,
+  });
+
+  /// [color] is for rendering animated icon with respected color.
+  /// Default Value: [Colors.black]
+  final Color color;
+
+  /// [height] is for rendering animated icon with respected height.
+  /// Default Value: [20]
+  final double? height;
+
+  /// [width] is for rendering animated icon with respected width.
+  /// Default Value: [20]
+  final double? width;
+
+  /// [riveIcon] is for rendering rive animated icon with respect to the passed icon.
+  /// it's a required parameter for [RiveAnimatedIcon]
+  final RiveIcon riveIcon;
+
+  /// [onTap] is a callback which is sent by user and it's used to perform actions on tap.
+  /// it's an optional parameter for [RiveAnimatedIcon]
+  final VoidCallback? onTap;
+
+  /// [onHover] is a callback which is sent by user depend on his requirements.
+  /// it's an  optional parameter for [RiveAnimatedIcon]
+  final Function? onHover;
+
+  /// [loopAnimation] is a boolean to set animation on loop or not.
+  /// Default Value: false
+  final bool? loopAnimation;
+
+
+
+
+  @override
+  State<RiveAnimatedIcon> createState() => _RiveAnimatedIconState();
+}
+
+class _RiveAnimatedIconState extends State<RiveAnimatedIcon> {
+  @override
+  Widget build(BuildContext context) {
+    RiveAsset icon = widget.riveIcon.getRiveAsset();
+    return InkWell(
+      onTap: (){
+        icon.input!.change(true);
+        Future.delayed(const Duration(seconds: 1),(){
+          icon.input!.change(false);
+        });
+        widget.onTap;
+      },
+      onHover: (_){
+        icon.input!.change(true);
+        Future.delayed(const Duration(seconds: 1),(){
+          icon.input!.change(false);
+        });
+        widget.onHover;
+      },
+      child: SizedBox(
+        height: widget.height,
+        width: widget.width,
+        child: RiveAnimation.asset(
+          icon.src,
+          artboard: icon.artboard,
+          onInit: (artboard){
+            StateMachineController controller =
+            RiveUtil.getRiveController(artboard,
+              stateMachineName: icon.stateMachineName,
+            );
+            icon.input = controller.findSMI("active") as SMIBool;
+
+            artboard.forEachComponent((child) {
+              if(child is Shape){
+                final Shape shape = child;
+                if(shape.name == icon.shapeStrokeTitle){
+                  shape.strokes.first.paint.color = widget.color;
+                }else if(shape.name == icon.shapeFillTitle){
+                  shape.fills.first.paint.color = widget.color;
+                }
+              }
+            });
+            widget.loopAnimation == true? icon.input!.change(true): icon.input!.change(false);
+          },
+        ),
+      ),
+
+    );
+  }
+}
